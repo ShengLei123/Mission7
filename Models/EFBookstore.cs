0b973228fdf7a5404d8443447c8eebd21f3e1f23
ShengLei123 <starkray666666@gmail.com>
2022-02-16 23:09:21 -0700

    Add project files.

---
+++ Models/EFBookstore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission7.Models
+{
+    public class EFBookstore:IBookstore
+    {
+        private BookstoreContext context { get; set; }
+        public EFBookstore (BookstoreContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Book> Books => context.Books;
+    }
+}
